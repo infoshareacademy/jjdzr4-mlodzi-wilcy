47e726589468e71ec743dc84cdd260d9c8c07e0b
Milosz Wozniak <milosz.wozniak@gmail.com>
2021-09-26 13:35:32 +0200

    Add project name into sout

--- myFit-consoleApp/src/main/java/com/infoshare/App.java
+++ myFit-consoleApp/src/main/java/com/infoshare/App.java
@@ -1,13 +1,7 @@
 package com.infoshare;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("myFit app :)");
     }
 }
