d93d4571ecde9323d82470d9424b0fb0c1022121
lyingparachute <igor1501@o2.pl>
2021-10-01 18:08:33 +0200

    Added text Interface

--- myFit-consoleApp/src/main/java/com/infoshare/Main.java
+++ myFit-consoleApp/src/main/java/com/infoshare/Main.java
@@ -1,7 +1,9 @@
 package com.infoshare;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("myFit app :)");
+        Interface anInterface = new Interface();
+        anInterface.defaultInterface();
     }
 }
---
+++ myFit-consoleApp/src/main/java/com/infoshare/UsersDataBase.java
@@ -0,0 +1,32 @@
+package com.infoshare;
+
+import java.util.Scanner;
+
+public class UsersDataBase {
+    public void logInUser(){
+        String name;
+        String password;
+        System.out.println("Hello. You can Log In into a system.");
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        System.out.println("Enter your password:");
+        scanner = new Scanner(System.in);
+        password = scanner.nextLine();
+
+        System.out.println("User " + name + " is logged in.");
+    }
+
+    public void addUser(){
+        String name;
+        String password;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name for new user:");
+        name = scanner.nextLine();
+        System.out.println("Enter password for new user:");
+        scanner = new Scanner(System.in);
+        password = scanner.nextLine();
+
+        System.out.println("New user: " + name + " added.");
+    }
+}
