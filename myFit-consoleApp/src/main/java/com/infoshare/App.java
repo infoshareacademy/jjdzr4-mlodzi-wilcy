6979c2718908e43329f979e4cce4479c087177fc
Milosz Wozniak <milosz.wozniak@gmail.com>
2021-10-03 12:11:45 +0200

    Use try and catch while saving data to the file

--- myFit-consoleApp/src/main/java/com/infoshare/App.java
+++ myFit-consoleApp/src/main/java/com/infoshare/App.java
@@ -4,15 +4,34 @@ import com.google.gson.Gson;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("myFit app :)");
 
         DummyUser dummyUser = new DummyUser();
         Gson gson = new Gson();
 
-        gson.toJson(dummyUser, new FileWriter("src/main/resources/dummy.json"));
-        gson.toJson(123.45, new FileWriter("src/main/resources/123.json"));
+        FileWriter firstFile = null;
+        try {
+            firstFile = new FileWriter("src/main/resources/dummy.json");
+            gson.toJson(dummyUser, firstFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileWriter secondFile = null;
+        try {
+            secondFile = new FileWriter("src/main/resources/123.json");
+            gson.toJson(123.45, secondFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (Writer writer = new FileWriter("src/main/resources/dummy2.json")) {
+            gson.toJson(dummyUser, writer);
+        } catch (IOException e) {
+            System.out.println("Exception during saving json file: " + e.getMessage());
+        }
     }
 }
