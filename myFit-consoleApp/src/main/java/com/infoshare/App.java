ef46cc06dc114153a008a0c5543ec1fe87e0e9aa
Milosz Wozniak <milosz.wozniak@gmail.com>
2021-10-03 12:37:46 +0200

    Add ready method write to json file

--- myFit-consoleApp/src/main/java/com/infoshare/App.java
+++ myFit-consoleApp/src/main/java/com/infoshare/App.java
@@ -4,14 +4,8 @@ import static com.infoshare.Utils.FileUtils.writeJsonToFile;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("myFit app :)");
 
-        DummyUser dummyUser = new DummyUser();
-
-        String pathToSave = "src/main/resources/dummy.json";
-        writeJsonToFile(dummyUser, pathToSave);
-
-//        Interface anInterface = new Interface();
+        //        Interface anInterface = new Interface();
 //        anInterface.defaultInterface();
     }
 }
