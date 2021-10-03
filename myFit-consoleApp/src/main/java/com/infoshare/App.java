b42522df5e38c82ddf57d319fd11138b78a1aebf
lyingparachute <igor1501@o2.pl>
2021-10-03 12:00:53 +0200

    Changed name to App

--- myFit-consoleApp/src/main/java/com/infoshare/App.java
+++ myFit-consoleApp/src/main/java/com/infoshare/App.java
@@ -1,6 +1,6 @@
 package com.infoshare;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("myFit app :)");
         Interface anInterface = new Interface();
