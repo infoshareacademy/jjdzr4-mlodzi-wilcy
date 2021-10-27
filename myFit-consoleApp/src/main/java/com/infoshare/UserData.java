a0bb480d35069f111b6292d6fade04a15df937da
Mateusz Lesisz <m.lesisz97@wp.pl>
2021-10-27 18:10:44 +0200

    repair setName

--- myFit-consoleApp/src/main/java/com/infoshare/UserData.java
+++ myFit-consoleApp/src/main/java/com/infoshare/UserData.java
@@ -1,6 +1,8 @@
 package com.infoshare;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserData {
     private String name;
@@ -11,7 +13,7 @@ public class UserData {
     private String levelOfJobActivity;
     private String levelOfPrivateActivity;
 
-    public void fillUserData() {
+    public void fillUserData(){
         System.out.println("Before enter fill your personal information");
         setName();
         setGender();
@@ -24,10 +26,23 @@ public class UserData {
 
 
     public void setName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Type your name:");
-        this.name = scanner.nextLine();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Type your name:");
+            this.name = scanner.nextLine();
+            Pattern p = Pattern.compile("\\d+");
+            Matcher m = p.matcher(name);
+            if(m.find() == true) {
+                String i = m.group();
+                System.out.println("Type correct name.");
+            }
+            else{
+                break;
+            }
+
+        }
     }
+
 
     public void setGender() {
         System.out.println("Your gender:");
@@ -50,7 +65,11 @@ public class UserData {
                 System.out.println("Your age:");
                 Scanner scanner = new Scanner(System.in);
                 this.age = scanner.nextInt();
-                break;
+                if (this.age <= 0) {
+                    System.out.println("Enter correct value.");
+                } else {
+                    break;
+                }
             } catch (Exception e) {
                 System.out.println("Write your age correctly.");
             }
@@ -63,7 +82,11 @@ public class UserData {
                 System.out.println("Your height");
                 Scanner scanner = new Scanner(System.in);
                 this.height = scanner.nextInt();
-                break;
+                if (this.height <= 0) {
+                    System.out.println("Enter correct value.");
+                } else {
+                    break;
+                }
             } catch (Exception e) {
                 System.out.println("Write your hight in centimeters.");
             }
@@ -76,7 +99,11 @@ public class UserData {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Your weight");
                 this.weight = scanner.nextDouble();
-                break;
+                if (this.weight <= 0) {
+                    System.out.println("Enter correct value.");
+                } else {
+                    break;
+                }
             } catch (Exception e) {
                 System.out.println("Write your weight correctly");
             }
