656a7fbb60231c895e3201f41d657922a375927f
Mateusz Lesisz <m.lesisz97@wp.pl>
2021-11-21 13:00:30 +0100

    app skeleton

---
+++ src/test/java/com/infoshare/myfitwebapp/MyFitWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoshare.myfitwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
