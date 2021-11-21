5900052aa27a857cb870d6e1a9d73d71d71bba98
Mateusz Lesisz <m.lesisz97@wp.pl>
2021-11-21 14:08:39 +0100

    add CPM service

---
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/model/UserData.java
@@ -0,0 +1,100 @@
+package com.infoshare.myfitwebapp.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserData {
+    //    UserLoginData userLoginData = new UserLoginData();
+    private String name;
+    private String gender;
+    private int age;
+    private int height;
+    private double weight;
+    private String levelOfJobActivity;
+    private String levelOfPrivateActivity;
+    private double weightGoal;
+    private double PPM;
+    private double CPM;
+//    public Diet diet;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getLevelOfJobActivity() {
+        return levelOfJobActivity;
+    }
+
+    public void setLevelOfJobActivity(String levelOfJobActivity) {
+        this.levelOfJobActivity = levelOfJobActivity;
+    }
+
+    public String getLevelOfPrivateActivity() {
+        return levelOfPrivateActivity;
+    }
+
+    public void setLevelOfPrivateActivity(String levelOfPrivateActivity) {
+        this.levelOfPrivateActivity = levelOfPrivateActivity;
+    }
+
+    public double getWeightGoal() {
+        return weightGoal;
+    }
+
+    public void setWeightGoal(double weightGoal) {
+        this.weightGoal = weightGoal;
+    }
+
+    public double getPPM() {
+        return PPM;
+    }
+
+    public void setPPM(double PPM) {
+        this.PPM = PPM;
+    }
+
+    public double getCPM() {
+        return CPM;
+    }
+
+    public void setCPM(double CPM) {
+        this.CPM = CPM;
+    }
+}
