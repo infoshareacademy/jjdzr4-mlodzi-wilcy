773e982459b8cbaa9fa5a87fc97a1dd0c12bda77
Milosz Wozniak <milosz.wozniak@gmail.com>
2021-11-28 13:38:47 +0100

    Add dummy ProductController

---
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.infoshare.myfitwebapp.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Component
+public class ProductController {
+
+//    @GetMapping("/user/products")
+}
