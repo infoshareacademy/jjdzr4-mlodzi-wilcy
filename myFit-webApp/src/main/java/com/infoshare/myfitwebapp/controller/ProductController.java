7a87641344849d3ab99d29164e4a47001034f45e
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-01-14 18:53:24 +0100

    I think task done

--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/ProductController.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/ProductController.java
@@ -1,21 +1,12 @@
 package com.infoshare.myfitwebapp.controller;
 
 import com.infoshare.myfitwebapp.model.ProductData;
-import com.infoshare.myfitwebapp.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
-
-    final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
 
     @GetMapping("user/product/new")
     public String getProduct(Model model) {
@@ -23,9 +14,4 @@ public class ProductController {
         return "product-new";
     }
 
-    @PostMapping("user/product/new")
-    public String sendTask(@ModelAttribute("task") ProductData productData) {
-        return "product-succes";
-
-    }
 }
