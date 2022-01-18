96d8e273ed801ffd5a6377f04ed58c9fcdcf6e0d
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2022-01-18 10:47:28 +0100

    add controller with check if form has errors

--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/ProductController.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/ProductController.java
@@ -3,7 +3,12 @@ package com.infoshare.myfitwebapp.controller;
 import com.infoshare.myfitwebapp.model.ProductData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class ProductController {
@@ -12,6 +17,18 @@ public class ProductController {
     public String getProduct(Model model) {
         model.addAttribute("product", new ProductData());
         return "product-new";
+    }
+
+    @PostMapping("user/product/new")
+    public String addProduct(@Valid @ModelAttribute("product") ProductData product, Errors errors) {
+        if (errors.hasErrors()) {
+            return "product-new";
+        }
+
+       /* TODO
+            service.add(product);*/
+
+        return "redirect:/";
     }
 
 }
