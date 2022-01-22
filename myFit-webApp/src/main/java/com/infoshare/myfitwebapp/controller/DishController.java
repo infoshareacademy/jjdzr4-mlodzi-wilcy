6f45ab8e60d4364c6ae99b03f2f2947e0bc78454
Milosz Wozniak <milosz.wozniak@gmail.com>
2022-01-22 19:03:30 +0100

    Add basic add dish form and dish controller

--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/DishController.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/DishController.java
@@ -2,7 +2,13 @@ package com.infoshare.myfitwebapp.controller;
 
 import com.infoshare.myfitwebapp.model.DishData;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class DishController {
@@ -10,5 +16,23 @@ public class DishController {
     @GetMapping("dishes")
     public DishData getAllDishes() {
         return null;
+    }
+
+    @GetMapping("user/dish/new")
+    public String getProduct(Model model) {
+        model.addAttribute("dish", new DishData());
+        return "dish-new";
+    }
+
+    @PostMapping("user/dish/new")
+    public String addProduct(@Valid @ModelAttribute("dish") DishData dish, Errors errors) {
+        if (errors.hasErrors()) {
+            return "dish-new";
+        }
+
+       /* TODO
+            service.add(dish);*/
+
+        return "redirect:/";
     }
 }
