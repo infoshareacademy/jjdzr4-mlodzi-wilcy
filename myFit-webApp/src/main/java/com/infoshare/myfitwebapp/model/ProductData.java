008da8235bb5279ce79c0a5db4cfb3831921f1a0
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2022-01-24 12:10:03 +0100

    add missed getter

--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/model/ProductData.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/model/ProductData.java
@@ -29,6 +29,14 @@ public class ProductData {
     @Column(nullable = false)
     private double proteinPer100g;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
