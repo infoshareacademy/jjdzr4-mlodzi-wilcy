873f8926477ae49cc73d7f514ed709df461f2fde
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2022-01-28 22:36:34 +0100

    temp commit

--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/UserController.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/controller/UserController.java
@@ -1,11 +1,29 @@
 package com.infoshare.myfitwebapp.controller;
 
+import com.infoshare.myfitwebapp.model.UserData;
+import com.infoshare.myfitwebapp.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.Collection;
 
 @Controller
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService service) {
+        this.userService = service;
+    }
 
     @GetMapping("user")
     public String getUser() {
@@ -13,14 +31,51 @@ public class UserController {
     }
 
     @PostMapping("goToUserEdit")
-    public String goToEdit(){
+    public String goToEdit() {
         return "redirect:/user/edit";
     }
 
+/*    @GetMapping("user/edit")
+    public String getUserEdit(Model model, Principal principal) {
+        model.addAttribute("user", getUserName(principal));
+        return "edit-user";
+    }*/
+
     @GetMapping("user/edit")
-    public String getUserEdit() {
+    public String getUserEdit(Model model, @AuthenticationPrincipal UserData user) {
+        model.addAttribute("user", user);
         return "edit-user";
     }
+
+    private String getUserName(Principal principal) {
+        if (principal == null) {
+            return "anonymous";
+        } else {
+
+            final UserDetails currentUser = (UserDetails) ((Authentication) principal).getPrincipal();
+            Collection<? extends GrantedAuthority> authorities = currentUser.getAuthorities();
+            for (GrantedAuthority grantedAuthority : authorities) {
+                System.out.println(grantedAuthority.getAuthority());
+            }
+
+            ((Authentication) principal).getPrincipal();
+
+            return principal.getName();
+        }
+    }
+
+    /*@GetMapping("editUser")
+    public String getUserCurrentData(Model model, Principal principal) {
+        String userName = principal.getName();
+        model.addAttribute("user", userService.getByName(userName));
+        return "edit-user";
+    }
+
+    @PutMapping("edit-user")
+    public String updateUserData() {
+
+        return "redirect:/user";
+    }*/
 
 }
 
--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/service/UserService.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/service/UserService.java
@@ -1,6 +1,8 @@
 package com.infoshare.myfitwebapp.service;
 
+import com.infoshare.myfitwebapp.model.UserData;
 import com.infoshare.myfitwebapp.model.UserLogin;
+import com.infoshare.myfitwebapp.repository.UserDataRepository;
 import com.infoshare.myfitwebapp.repository.UserLoginRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +11,12 @@ import java.util.List;
 @Service
 public class UserService {
 
-    private UserLoginRepository userRepository;
+    private final UserLoginRepository userRepository;
+    private final UserDataRepository userDataRepository;
 
-    public UserService(UserLoginRepository userRepository) {
+    public UserService(UserLoginRepository userRepository, UserDataRepository userDataRepository) {
         this.userRepository = userRepository;
+        this.userDataRepository = userDataRepository;
     }
 
     public Iterable<UserLogin> save(List<UserLogin> users) {
@@ -21,5 +25,11 @@ public class UserService {
 
     public UserLogin save(UserLogin userLogin) {
         return userRepository.save(userLogin);
+    }
+
+    public UserData getByName(String userName) {
+        UserLogin userLogin = userRepository.findByUsername(userName);
+        Long userId = userLogin.getId();
+        return userDataRepository.getById(userId);
     }
 }
