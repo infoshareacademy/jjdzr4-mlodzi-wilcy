5fc2343bc944b92a111c74f9c5175e79772f66d2
lyingparachute <igor1501@o2.pl>
2022-02-09 21:20:35 +0100

    New dish page v7

--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/SecurityConfig.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
                 http.authorizeRequests()
-                .antMatchers("/", "/register", "/webjars/**").permitAll()
+                .antMatchers("/", "/register", "/webjars/**", "/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
