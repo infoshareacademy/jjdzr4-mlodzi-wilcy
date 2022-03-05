aef3405855599c6ec3e438befe58adce19a43eab
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2022-03-05 13:23:25 +0100

    clean up cesurity config

--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/config/SecurityConfig.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/config/SecurityConfig.java
@@ -1,13 +1,10 @@
 package com.infoshare.myfitwebapp.config;
 
-import com.infoshare.myfitwebapp.service.MyUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-                http.authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/", "/register", "/webjars/**", "/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -30,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
