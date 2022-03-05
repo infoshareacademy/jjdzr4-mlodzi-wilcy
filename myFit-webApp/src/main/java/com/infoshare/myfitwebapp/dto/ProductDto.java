259cdff8d2cfe174e3dd2124023a2b0a39facfb9
lyingparachute <igor1501@o2.pl>
2022-03-05 15:27:29 +0100

    Add products view with search

--- myFit-webApp/src/main/java/com/infoshare/myfitwebapp/dto/ProductDto.java
+++ myFit-webApp/src/main/java/com/infoshare/myfitwebapp/dto/ProductDto.java
@@ -2,24 +2,26 @@ package com.infoshare.myfitwebapp.dto;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.PositiveOrZero;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
-
-    public ProductDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    private int kcalPer100g;
+    private double fatPer100g;
+    private double carbohydratesPer100g;
+    private double proteinPer100g;
 
     @Override
     public String toString() {
